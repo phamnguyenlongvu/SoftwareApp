f15615f5c35afd37394c6713fbe55b9c2cef46f4
Phmngnlgv <62500643+phamnguyenlongvu@users.noreply.github.com>
2022-07-20 12:11:04 +0700

    Simple SVVM Pattern User UI with Hard Data.

---
+++ SimpleMVVMModel/SimpleMVVMModel/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using SimpleMVVMModel.View;
+using SimpleMVVMModel.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SimpleMVVMModel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Mainpage window = new Mainpage();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.ShowDialog();
+        }
+    }
+}
---
+++ SimpleMVVMModel/SimpleMVVMModel/View/Mainpage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleMVVMModel.View
+{
+    /// <summary>
+    /// Interaction logic for Mainpage.xaml
+    /// </summary>
+    public partial class Mainpage : Window
+    {
+        public Mainpage()
+        {
+            InitializeComponent();
+        }
+    }
+}
